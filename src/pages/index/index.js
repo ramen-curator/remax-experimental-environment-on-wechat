44fadef7f03d46bd90edb013f55b3fcdb29118a3
赖铭志 <ramen.curator@gmail.com>
2021-12-23 23:37:39 +0800

    complete to show question

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -11,7 +11,7 @@ const fakeAPI = () => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       res(data);
-    }, 3000);
+    }, 1000);
   });
 };
 
@@ -21,13 +21,43 @@ export default () => {
     fakeAPI().then((res) => setDataList(res));
   }, []);
 
+  const [enabled, setEnabled] = React.useState(false);
+  // 不这么做，loading只能拉到Ad.的范围。
+    //  第一次enable后，setEnabled的权力应该转移给onPageScroll
+  const [isFirstRender, setIsFirstRender] = React.useState(true); 
+  useNativeEffect(() => {
+    if (dataList.length > 0 && isFirstRender) {
+      setIsFirstRender(false)
+      setEnabled(true);
+    }
+  }, [dataList]);
+  // 不这么做，划下去后，想划上来就会触发下拉加载。
+  usePageEvent("onPageScroll", (e) => {
+    setEnabled(e.scrollTop <= 0);
+  });
+
+  const [triggered, setTriggered] = React.useState(true);
+
   return (
     <View className="container">
       <View className="header"></View>
-      <ScrollView scrollY>
+      <ScrollView
+        scrollY
+        refresherEnabled={enabled}
+        refresherTriggered={triggered}
+        onRefresherRefresh={() => {
+          setTriggered(true);
+          fakeAPI().finally(() => {
+            setTriggered(false);
+          });
+        }}
+      >
         <View className="main">
-          {dataList.map((v) => (
-            <View className="card">Card {v}</View>
+          <View className="ad">Ad.</View>
+          {dataList.map((v, i) => (
+            <View className="card" key={i.toString()}>
+              Card {v}
+            </View>
           ))}
         </View>
       </ScrollView>
