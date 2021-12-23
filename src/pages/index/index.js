5dc4e7ff660d244a8e0c0ef7b19751cb9ac0d320
赖铭志 <ramen.curator@gmail.com>
2021-12-23 23:11:42 +0800

    init easy layout

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,36 @@
 import * as React from "react";
-import { View } from "remax/wechat";
+import { View, ScrollView, GenericEvent } from "remax/wechat";
+import { useNativeEffect } from "remax";
+import { usePageEvent } from "remax/macro";
 
 import "./index.css";
 
+const data = new Array(10).fill(null).map((v, i, a) => (a[i] = i));
+
+const fakeAPI = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(data);
+    }, 3000);
+  });
+};
+
 export default () => {
-  return <View>Hello World</View>;
+  const [dataList, setDataList] = React.useState([]);
+  useNativeEffect(() => {
+    fakeAPI().then((res) => setDataList(res));
+  }, []);
+
+  return (
+    <View className="container">
+      <View className="header"></View>
+      <ScrollView scrollY>
+        <View className="main">
+          {dataList.map((v) => (
+            <View className="card">Card {v}</View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
 };
